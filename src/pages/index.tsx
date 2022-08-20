0ec1a037a7164fba8b023f34574d174384ccb44b
Josh Benfield <josh@benfield.org>
2022-08-20 17:38:20 -0400

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <h1>Hello, World!</h1>
+            <p>This is a starting point for a Next.js project...</p>
+        </>
+    );
+};
+
+export default Home;
